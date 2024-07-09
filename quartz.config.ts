fef22b57102f303eabd8661d21eddffbf576ca82
k4r4b3y <user@host.localdomain>
2024-07-09 14:05:59 +0300

    Translate the pageTitle

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Monero Garden",
+    pageTitle: "Monero Bahçesi",
     enableSPA: true,
     enablePopovers: true,
     analytics: null,
